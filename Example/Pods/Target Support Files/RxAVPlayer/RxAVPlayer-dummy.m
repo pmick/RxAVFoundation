f5012b775510b50d58beaf987209fab284820feb
Patrick Mick <patrickmick1@gmail.com>
2016-04-05 15:28:42 -0700

    Removing cached but now ignored files

--- Example/Pods/Target Support Files/RxAVPlayer/RxAVPlayer-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_RxAVPlayer : NSObject
-@end
-@implementation PodsDummy_RxAVPlayer
-@end
--- Example/Pods/Target Support Files/RxAVPlayer/RxAVPlayer-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double RxAVPlayerVersionNumber;
-FOUNDATION_EXPORT const unsigned char RxAVPlayerVersionString[];
-
