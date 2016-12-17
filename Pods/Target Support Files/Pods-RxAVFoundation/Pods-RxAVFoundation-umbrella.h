2cf3d2b7e61b8bb805dfe277afdd309000a8daff
Patrick Mick <patrickmick1@gmail.com>
2016-12-17 13:54:28 -0800

    Feature/get circle ci passing (#3)
    * Adding a circle yml
    
    * Switching to using scan to run tests
    
    * Adding a step to the circle yml to update pods
    
    * Just running pod install directly
    
    * Adding an output directory for test reports
    
    * Updating test output directory and removing pod install because it shouldn't be needed
    
    * Removing podfile.lock from the git ignore to try to get circle ci builds passing with cached pods
    
    * Removing the pods directories from the gitignore because circle ci is installing pods in the example rather than the framework project

---
+++ Pods/Target Support Files/Pods-RxAVFoundation/Pods-RxAVFoundation-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_RxAVFoundationVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_RxAVFoundationVersionString[];
+
---
+++ Pods/Target Support Files/Pods-RxAVFoundationTests/Pods-RxAVFoundationTests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_RxAVFoundationTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_RxAVFoundationTestsVersionString[];
+
