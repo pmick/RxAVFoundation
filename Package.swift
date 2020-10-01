b5d30329a26a6a23851c6525e6977de5dd981704
pmick <patrickmick1@gmail.com>
2020-09-30 22:38:25 -0700

    Add package manifest

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "RxAVFoundation",
+    platforms: [
+        .macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v3)
+    ],
+    products: [
+        .library(
+            name: "RxAVFoundation",
+            targets: ["RxAVFoundation"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "RxAVFoundation",
+            dependencies: ["RxSwift", "RxCocoa"]),
+        .testTarget(
+            name: "RxAVFoundationTests",
+            dependencies: ["RxAVFoundation"]),
+    ]
+)
--- Pods/Target Support Files/Pods-RxAVFoundation/Pods-RxAVFoundation-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RxAVFoundation : NSObject
-@end
-@implementation PodsDummy_Pods_RxAVFoundation
-@end
