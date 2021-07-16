49239b49ca0702f5cb95dfa06696f59e90956228
tackgyu <spinion13@gmail.com>
2021-07-16 11:06:29 +0900

    Support RxSwift 6

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["RxAVFoundation"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.0.0"),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.0.0"),
     ],
     targets: [
         .target(
